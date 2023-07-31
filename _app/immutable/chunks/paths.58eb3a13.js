d6e8f9ac7d63e5e06d04ce9d0a6f2ea303e6021d
michealparks <michealparks1989@gmail.com>
2023-07-31 14:43:37 +0000

    Deploying to gh-pages from @ michealparks/threlte-xr@ad516480c0a3a16d211b196c0525f362b6f8c239 🚀

---
+++ _app/immutable/chunks/paths.58eb3a13.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1y3fa6z)==null?void 0:s.base)??"/threlte-xr";var a;const t=((a=globalThis.__sveltekit_1y3fa6z)==null?void 0:a.assets)??e;export{t as a,e as b};
